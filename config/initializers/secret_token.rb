e199399f97c772512651297ac613359378ec2cab
Evan Dorfman <edorfs@gmail.com>
2012-10-26 23:22:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harmonikv1::Application.config.secret_token = 'c5bc6adc9ff1ad69d47187792f8a67d47bdeda74575a3022b0f474c2d614f68e577d71483db7c8ff152f29dfba8f23c8f5c4f1a32be5d4676024646881e9dda1'
